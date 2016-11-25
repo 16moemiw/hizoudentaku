af1ae07722d776ca657ff496f25a99f3c693fd20
Moemi Watanabe <moemiwatanabe@anhus-macbook-air.local>
2016-11-25 19:27:33 +0900

    Initial Commit

---
+++ 秘蔵電卓/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  秘蔵電卓
+//
+//  Created by Moemi Watanabe on 11/25/16.
+//  Copyright © 2016 Moemi Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
